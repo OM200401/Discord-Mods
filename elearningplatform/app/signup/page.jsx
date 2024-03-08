ebd91a5e05e6780fb0457d5f9166775fbf194f30
OM200401 <ommistry012004@gmail.com>
2024-03-07 17:43:23 -0800

    edited the signup page and added temp course cards

--- elearningplatform/app/signup/page.jsx
+++ elearningplatform/app/signup/page.jsx
@@ -42,7 +42,11 @@ export default function SignUpPage() {
                         <form onSubmit={handleSubmit} className="divide-y divide-gray-200">
                             <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                 <div className="flex flex-col">
-                                    <label className="leading-loose">Name</label>
+                                    <label className="leading-loose">First Name</label>
+                                    <input type='input' onChange={e => setName(e.target.value)} className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Name" value={name} />
+                                </div>
+                                <div className="flex flex-col">
+                                    <label className="leading-loose">Last Name</label>
                                     <input type='input' onChange={e => setName(e.target.value)} className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Name" value={name} />
                                 </div>
                                 <div className="flex flex-col">
