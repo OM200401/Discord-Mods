1ef3162a27241aaf29c442bda4467f9733425555
OM200401 <ommistry012004@gmail.com>
2024-03-13 14:11:48 -0700

    added more dependencies and fixed bugs

--- elearningplatform/app/page.jsx
+++ elearningplatform/app/page.jsx
@@ -29,10 +29,6 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold pt-10 font-mono">
           Welcome to our E-Learning Platform
-          <h2>{students.length > 0 && students.map(student => (
-            <h1 className=' text-white'>{student.Name}</h1>
-          ))}</h2>
-
         </h1>
         <p className="mt-3 text-2xl font-mono">
           Join us and explore the world of knowledge.
@@ -46,10 +42,10 @@ export default function Home() {
           </a>
         </div>
 
-        <div className="flex mt-6 justify-center">
+        {/* <div className="flex mt-6 justify-center">
            <h1>Below is from firebase to prove it works</h1>
             <h2></h2>
-        </div>
+        </div> */}
 
 
       </main>
