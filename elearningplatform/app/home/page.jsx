661797750209306d84d029e61a61b35572bb6aca
OM200401 <ommistry012004@gmail.com>
2024-03-13 15:17:36 -0700

    added routing for Course Page

--- elearningplatform/app/home/page.jsx
+++ elearningplatform/app/home/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from "next/link";
 import Sidebar from "../components/Sidebar"; 
 import CourseCard from "../components/CourseCard";
 import { useState, useEffect } from "react";
@@ -13,6 +14,8 @@ export default function home(){
     const [userName, setUserName] = useState('non');
     const [user,setUser] = useState();
 
+// create a new function that will get the CourseCard info on clicking it and then go to the
+// backend and get info about that course to redirect to the particular Course page 
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, async (user) => {
@@ -47,10 +50,10 @@ export default function home(){
         <div className="flex flex-col md:flex-row ml-80">
             <Sidebar userName={ userName } />
             <div className="mt-4 md:mt-0 md:ml-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 p-4 md:p-8">
-                <CourseCard courseCode="COSC 310" courseName="Software Engineering" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_gaZXWjn_qJVUXTVnHnGIPRka3psRSJgShg&usqp=CAU" />               
-                <CourseCard courseCode="COSC 304" courseName="Introduction to Databases" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_gaZXWjn_qJVUXTVnHnGIPRka3psRSJgShg&usqp=CAU" />               
-                <CourseCard courseCode="PHIL 331" courseName="Computer Ethics" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_gaZXWjn_qJVUXTVnHnGIPRka3psRSJgShg&usqp=CAU" />               
-                <CourseCard courseCode="PSYO 111" courseName="Introduction to Psychology I" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_gaZXWjn_qJVUXTVnHnGIPRka3psRSJgShg&usqp=CAU" />               
+                <Link href={`/[courseCode]?courseCode=COSC310`}><CourseCard courseCode="COSC 310" courseName="Software Engineering" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_gaZXWjn_qJVUXTVnHnGIPRka3psRSJgShg&usqp=CAU" /></Link>
+                <Link href={`/[courseCode]?courseCode=COSC304`}><CourseCard courseCode="COSC 304" courseName="Introduction to Databases" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_gaZXWjn_qJVUXTVnHnGIPRka3psRSJgShg&usqp=CAU" /></Link>
+                <Link href={`/[courseCode]?courseCode=PHIL331`}><CourseCard courseCode="PHIL 331" courseName="Computer Ethics" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_gaZXWjn_qJVUXTVnHnGIPRka3psRSJgShg&usqp=CAU" /></Link>
+                <Link href={`/[courseCode]?courseCode=PSYO111`}><CourseCard courseCode="PSYO 111" courseName="Introduction to Psychology I" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_gaZXWjn_qJVUXTVnHnGIPRka3psRSJgShg&usqp=CAU" /></Link>
             </div>
         </div>
     );
