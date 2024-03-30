6fdb8731d57e3450b3914da99f88d8fb49928fae
aaronbanerjee123 <aaronbaner@outlook.com>
2024-03-30 15:11:09 -0700

    can upload quiz to database

--- app/[courseCode]/addAssignments/page.jsx
+++ app/[courseCode]/addAssignments/page.jsx
@@ -1,10 +1,14 @@
-"use client";
+
+'use client'
 import { useState } from 'react';
 import Sidebar from '../../components/Sidebar';
+import db from '../../lib/firebase';
+import {doc,setDoc} from 'firebase/firestore';
 
 export default function Assignments() {
     const [showForm, setShowForm] = useState(false);
-    const [questions, setQuestions] = useState([{ text: '', options: ['Option #1', 'Option #2'] }]);
+    const [quizTitle, setQuizTitle] = useState('');
+    const [questions, setQuestions] = useState([{ text: '', options: ['Option #1', 'Option #2'], correctAnswer: null }]);
     const [weightage, setWeightage] = useState(0);
 
     const handleAddOption = (questionIndex) => {
@@ -21,13 +25,13 @@ export default function Assignments() {
     };
 
     const handleAddQuestion = () => {
-        setQuestions([...questions, { text: '', options: ['Option #1', 'Option #2'] }]);
+        setQuestions([...questions, { text: '', options: ['Option #1', 'Option #2'], correctAnswer: null }]);
     };
 
     const handleCorrectOptionChange = (questionIndex, optionIndex) => {
         setQuestions(questions.map((question, index) => {
             if (index === questionIndex) {
-                return { ...question, correctOption: optionIndex };
+                return { ...question, correctAnswer: optionIndex };
             }
             return question;
         }));
@@ -39,6 +43,20 @@ export default function Assignments() {
             setWeightage(value);
         } else {
             alert('Weightage must be a number between 0 and 100.');
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const quizCollectionRef = doc(db, 'quizzes',quizTitle);
+            setDoc(quizCollectionRef, {  questions });
+            // Optionally, you can reset the form after submission
+            setQuizTitle('');
+            setQuestions([{ text: '', options: ['Option #1', 'Option #2'], correctAnswer: null }]);
+            setShowForm(false);
+        } catch (error) {
+            console.error('Error adding quiz:', error);
         }
     };
 
@@ -56,61 +74,71 @@ export default function Assignments() {
                 {showForm && (
                     <>
                         <div className="flex flex-row items-center justify-center p-4 text-black font-semibold">
+                            <label className="mr-2">Quiz Title:</label>
+                            <input
+                                type="text"
+                                value={quizTitle}
+                                onChange={(e) => setQuizTitle(e.target.value)}
+                                className="border border-gray-300 px-4 py-2 rounded-md text-black"
+                                placeholder="Enter quiz title"
+                            />
+                        </div>
+                        <div className="flex flex-row items-center justify-center p-4 text-black font-semibold">
                             <label className="mr-2">Weightage of the quiz in the course:</label>
                             <input type="number" value={weightage} onChange={handleWeightageChange} min="0" max="100" />
                         </div>
                         {questions.map((question, questionIndex) => (
-                    
-                    <div key={questionIndex} className="mt-4">
-                        <input
-                            type="text"
-                            placeholder="Enter question"
-                            className="border border-gray-300 px-4 py-2 rounded-md text-black w-96 mb-4"
-                            value={question.text}
-                            onChange={(e) => setQuestions(questions.map((q, i) => {
-                                if (i === questionIndex) {
-                                    return { ...q, text: e.target.value };
-                                }
-                                return q;
-                            }))}
-                        />
-                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-4" onClick={() => handleDeleteQuestion(questionIndex)}>Delete Question</button>
-                        <h3 className="text-lg text-black font-semibold mb-2">Select Correct Option</h3>
-                        {question.options.map((option, optionIndex) => (
-                            <div key={optionIndex} className="flex flex-row justify-center items-center mt-2">
-                                <input
-                                    type="radio"
-                                    className='mx-10'
-                                    checked={question.correctOption === optionIndex}
-                                    onChange={() => handleCorrectOptionChange(questionIndex, optionIndex)}
-                                />
+                            <div key={questionIndex} className="mt-4">
                                 <input
                                     type="text"
-                                    value={option}
-                                    className="border border-gray-300 px-4 py-2 rounded-md text-black w-64 mr-4"
+                                    placeholder="Enter question"
+                                    className="border border-gray-300 px-4 py-2 rounded-md text-black w-96 mb-4"
+                                    value={question.text}
                                     onChange={(e) => setQuestions(questions.map((q, i) => {
                                         if (i === questionIndex) {
-                                            return { ...q, options: q.options.map((o, j) => {
-                                                if (j === optionIndex) {
-                                                    return e.target.value;
-                                                }
-                                                return o;
-                                            }) };
+                                            return { ...q, text: e.target.value };
                                         }
                                         return q;
                                     }))}
                                 />
-                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => setQuestions(questions.map((q, i) => {
-                                    if (i === questionIndex) {
-                                        return { ...q, options: q.options.filter((o, j) => j !== optionIndex) };
-                                    }
-                                    return q;
-                                }))}>Delete Option</button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-4" onClick={() => handleDeleteQuestion(questionIndex)}>Delete Question</button>
+                                <h3 className="text-lg text-black font-semibold mb-2">Select Correct Option</h3>
+                                {question.options.map((option, optionIndex) => (
+                                    <div key={optionIndex} className="flex flex-row justify-center items-center mt-2">
+                                        <input
+                                            type="radio"
+                                            className='mx-10'
+                                            checked={question.correctAnswer === optionIndex}
+                                            onChange={() => handleCorrectOptionChange(questionIndex, optionIndex)}
+                                        />
+                                        <input
+                                            type="text"
+                                            value={option}
+                                            className="border border-gray-300 px-4 py-2 rounded-md text-black w-64 mr-4"
+                                            onChange={(e) => setQuestions(questions.map((q, i) => {
+                                                if (i === questionIndex) {
+                                                    return { ...q, options: q.options.map((o, j) => {
+                                                        if (j === optionIndex) {
+                                                            return e.target.value;
+                                                        }
+                                                        return o;
+                                                    }) };
+                                                }
+                                                return q;
+                                            }))}
+                                        />
+                                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => setQuestions(questions.map((q, i) => {
+                                            if (i === questionIndex) {
+                                                return { ...q, options: q.options.filter((o, j) => j !== optionIndex) };
+                                            }
+                                            return q;
+                                        }))}>Delete Option</button>
+                                    </div>
+                                ))}
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={() => handleAddOption(questionIndex)}>Add Option</button>
                             </div>
                         ))}
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={() => handleAddOption(questionIndex)}>Add Option</button>
-                        </div>
-                        ))}
+                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={handleSubmit}>Submit Quiz</button>
                     </>
                 )}
             </div>
