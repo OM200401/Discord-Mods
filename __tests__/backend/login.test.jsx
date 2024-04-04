6f2a67d33e96cb83ae35085cc4c4e59a9cc81eb6
Y0WAIM0 <devstutya.52.2021@doonschool.com>
2024-04-03 22:40:36 -0700

    Created backend authentication testing

--- __tests__/backend/login.test.jsx
+++ __tests__/backend/login.test.jsx
@@ -1,28 +1,35 @@
 
-// import db from '../../app/lib/firebase';
-// import { collection, query, where, getDocs } from "firebase/firestore";
-// import { getAuth, onAuthStateChanged, signInWithEmailandPassword } from "firebase/auth";
-// import {auth} from '../lib/firebase';
+import db from '../../app/lib/firebase';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../../app/lib/firebase';
 
 
-// describe('Firebase Database Tests', () => {    
-//     test('Test wrong info signUp operation', async () => {
-//         let error='';
-//         try {
-//             await signInWithEmailandPassword('abcd@gmail.com','1');
-//         }catch(e){
-//             error = e.toString();
-//         }        
-//         expect(error).toBeDefined();
-//     });
+describe('Firebase Database Tests', () => {    
+    test('Test wrong info login operation', async () => {
+        let error='';
+        try {
+            await signInWithEmailandPassword('abcd@gmail.com','1');
+        }catch(e){
+            error = e.toString();
+        }        
+        expect(error).toBeDefined();
+    });
 
-//     test('Test correct info signUp operation', async () => {
-//         let auth = getAuth();
-//         signInWithEmailAndPassword(auth,'abcd@gmail.com','123456').then((user) => {
-//             const testuid = user.uid;
-//         });        
-//         // expect(isAuthenticated()).toBe(true);
-//         expect(user.uid).toEqual('e1BVk9Axbjg5dmC62XcjtDDBZcy2');
-//     });
+    test('Test correct login', async () => {        
+        const email = 'testuid1@gmail.com';
+        const password = '123456';
 
-// });
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;                   
+            expect(user.uid).toBeDefined(); // Ensure a user object is returned
+            expect(user.email).toEqual(email); // Ensure the returned user has the correct email
+            // Add more assertions as needed
+        } catch (error) {
+            // If login fails, fail the test
+            throw new Error('Login failed: ' + error.message);
+        }
+    });
+
+});
