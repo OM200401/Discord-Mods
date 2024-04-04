65d5fda1dfd214fc9c7731b784e1e2afc6efaa9d
Y0WAIM0 <devstutya.52.2021@doonschool.com>
2024-04-04 16:14:28 -0700

    Added login backendtest

---
+++ __tests__/backend/login.test.jsx
@@ -0,0 +1,35 @@
+
+import db from '../../app/lib/firebase';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../../app/lib/firebase';
+
+
+describe('Firebase Database Tests', () => {    
+    test('Test wrong info login operation', async () => {
+        let error='';
+        try {
+            await signInWithEmailandPassword('abcd@gmail.com','1');
+        }catch(e){
+            error = e.toString();
+        }        
+        expect(error).toBeDefined();
+    });
+
+    test('Test correct login', async () => {        
+        const email = 'testuid1@gmail.com';
+        const password = '123456';
+
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;                   
+            expect(user.uid).toBeDefined(); // Ensure a user object is returned
+            expect(user.email).toEqual(email); // Ensure the returned user has the correct email
+            // Add more assertions as needed
+        } catch (error) {
+            // If login fails, fail the test
+            throw new Error('Login failed: ' + error.message);
+        }
+    });
+
+});
