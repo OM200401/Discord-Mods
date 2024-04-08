5eb4b2c0c5afea151fca5c9a2c8e79757dab7994
Y0WAIM0 <devstutya.52.2021@doonschool.com>
2024-04-07 17:57:24 -0700

    Fixed enrollCourse spec

--- __test__/enrollCourse.spec.js
+++ __test__/enrollCourse.spec.js
@@ -2,37 +2,38 @@ import { test, expect } from '@playwright/test';
 
 test('should be able to enroll in a class', async ({ page }) => {
   test.setTimeout(120000);
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+
+  // Navigate to the index page
   await page.goto('http://localhost:3000/');
-  // Find an element with the text 'Get Started' and click on it to navigate to the login page
+  
+  // Click on "Get Started" to navigate to the login page
   await page.click('text=Get Started');
-  // The new URL should be "/login" (baseURL is used there)
-  await page.waitForNavigation(); // Wait for navigation to complete
+  
+  // Verify navigation to the login page
   await expect(page).toHaveURL('http://localhost:3000/login');
 
-  // Input email and password and click on the login button
-  await page.fill('input[type="email"]', 'abcd@gmail.com');
+  // Fill in email and password and click on the login button
+  await page.fill('input[type="email"]', 'endtoend@gmail.com');
   await page.fill('input[type="password"]', '123456');
   await page.click('button', { text: 'Login' });
 
-  // Optionally, you can add expectations to verify successful login
-  // For example, you can expect to navigate to a certain page after successful login
-  await page.waitForNavigation(); // Wait for navigation to complete
+  // Wait for successful login and navigation to student home page
+  await page.waitForNavigation();
   await expect(page).toHaveURL('http://localhost:3000/stuHome');
 
-  // Click on the "Browse" link
+  // Click on the "Browse" link to navigate to the course browsing page
   await page.click('text=Browse');
-
-  // The new URL should be "/browseCourses"
-  await page.waitForNavigation(); // Wait for navigation to complete
+  
+  // Verify navigation to the course browsing page
   await expect(page).toHaveURL('http://localhost:3000/browseCourses');
 
   // Click on "See more" to view course details
+  // Find the course with the title 'BIOL696' and click on the 'See more' button within it
   await page.click('text=See more');
-
+  
   // Wait for navigation to complete
   await page.waitForNavigation();
   
-  // The new URL should include the course code
-  await expect(page).toHaveURL('http://localhost:3000/browseCourses/BIOL696'); 
+  // The new URL should include the course code in the course details page
+  await expect(page.url()).toContain('/browseCourses/BIOL696'); 
 });
