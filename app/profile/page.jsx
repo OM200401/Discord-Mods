060ea45f4f7c2c1ea8fec54c8a6e0b10d8dcb185
aamirahmed2004 <syedaamir099@gmail.com>
2024-04-12 18:03:27 -0700

    Refactored profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,12 +1,14 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { auth } from '../lib/firebase'; 
-import { updateDoc, getDocs, query, where, collection } from 'firebase/firestore';
+import { updateDoc } from 'firebase/firestore';
 import { updatePassword } from 'firebase/auth';
 import { onAuthStateChanged, EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth';
 import Sidebar from '../views/Sidebar';
 import AdminSidebar from '../views/AdminSidebar';
-import db from '../lib/firebase';
+import { getStudentDoc } from '../utilities/StudentUtilities';
+import { getTeacherDoc } from '../utilities/TeacherUtilities';
+import { getAdminDoc } from '../utilities/AdminUtilities';
 
 export default function Profile() {
     // State variables
@@ -17,6 +19,7 @@ export default function Profile() {
         uid: "",
         userType:""
     }); // State for storing user input
+
     const [user,setUser] = useState(); // State for storing user
     const [userType,setUserType] = useState('user'); // State for storing user type
     const [isEditing, setIsEditing] = useState(false); // State for storing editing status
@@ -35,41 +38,26 @@ export default function Profile() {
         const unsubscribe = onAuthStateChanged(auth, async (user) => {
             if (auth.currentUser) {
                 setUser(auth.currentUser);
-                console.log(user);
                 console.log(user.uid);
-                const stuQuery = query(collection(db, 'students'), where('uid', '==', user.uid));
-                const stuSnapshot = await getDocs(stuQuery);
 
-                const teachQuery = query(collection(db, 'teachers'), where('uid', '==', user.uid));
-                const teachSnapshot = await getDocs(teachQuery);
+                const stuSnapshot = await getStudentDoc(user.uid);
+                const teacherSnapshot = await getTeacherDoc(user.uid);
+                const adminSnapshot = await getAdminDoc(user.uid);
 
-                const adminQuery = query(collection(db, 'admins'), where('uid', '==', user.uid));
-                const adminSnapshot = await getDocs(adminQuery);
-
-                if (!stuSnapshot.empty) {
-                    stuSnapshot.forEach((doc) => {
-                        console.log(doc.id, ' => ', doc.data());
-                        setUserInput(doc.data());
-                    });
-                }else if(!teachSnapshot.empty){
-                    teachSnapshot.forEach((doc) => {
-                        console.log(doc.id, ' => ', doc.data());
-                        setUserInput(doc.data());
-                    });
-                }else if(!adminSnapshot.empty){
-                    adminSnapshot.forEach((doc) => {
-                        console.log(doc.id, ' => ', doc.data());
-                        setUserInput(doc.data());
-                    });
-                }else {
-                    console.log('No such document!');
+                if(stuSnapshot.exists()){
+                    setUserInput(stuSnapshot.data());
+                } else if(teacherSnapshot.exists()){
+                    setUserInput(teacherSnapshot.data());
+                } else if(adminSnapshot.exists()){
+                    setUserInput(adminSnapshot.data());
+                } else {
+                    return;
                 }
             } else {
                 // User is signed out
                 console.log('No user is signed in.');
             }
         });
-
         // Cleanup subscription on unmount
         return () => unsubscribe();
     }, []);
@@ -95,56 +83,48 @@ export default function Profile() {
     const handleSave = async () => {
         // console.log("clicked");
         if(userInput.userType === "Student"){
-            const stu = query(collection(db, 'students'), where('uid', '==', userInput.uid));
-            const stuSnap = await getDocs(stu);
+            const stuSnap = await getStudentDoc(user.uid);
             // console.log("in if condition");
-            if(!stuSnap.empty){
-                const docRef = stuSnap.docs[0].ref;
+            if(stuSnap.exists()){
+                const docRef = stuSnap.ref;
                 console.log('Updating document:', docRef);
                 console.log('With data:', {
                     firstName: editedUserInput.firstName,
-                    lastName: editedUserInput.lastName,
-                    email: editedUserInput.email,
+                    lastName: editedUserInput.lastName
                 });
                 await updateDoc(docRef, {
                     firstName: editedUserInput.firstName,
-                    lastName: editedUserInput.lastName,
-                    email: editedUserInput.email,
+                    lastName: editedUserInput.lastName
                     // Add other fields as needed
                 });
             }
         }
         
         if(userInput.userType === "Teacher"){
-            const teacher = query(collection(db, 'teachers'), where('uid', '==', userInput.uid));
-            const teachSnap = await getDocs(teacher);
-            if(!teachSnap.empty){
-                const docRef = teachSnap.docs[0].ref;
+            const teachSnap = await getTeacherDoc(user.uid);
+            if(teachSnap.exists()){
+                const docRef = teachSnap.ref;
                 await updateDoc(docRef, {
                     firstName: editedUserInput.firstName,
-                    lastName: editedUserInput.lastName,
-                    email: editedUserInput.email,
+                    lastName: editedUserInput.lastName
                     // Add other fields as needed
                 });
             }
         }
 
         if(userInput.userType === "admin"){
-            const admin = query(collection(db, 'admins'), where('uid', '==', userInput.uid));
-            const adminSnap = await getDocs(admin);
-            if(!adminSnap.empty){
-                const docRef = adminSnap.docs[0].ref;
+            const adminSnap = await getAdminDoc(user.uid);
+            if(adminSnap.exists()){
+                const docRef = adminSnap.ref;
                 await updateDoc(docRef, {
                     firstName: editedUserInput.firstName,
                     lastName: editedUserInput.lastName,
-                    email: editedUserInput.email,
                     // Add other fields as needed
                 });
             }
         }
         
         setUserInput(editedUserInput);
-
         toggleEditing();
     };
 
@@ -168,8 +148,6 @@ export default function Profile() {
             console.error('Error updating password:', error);
         }
     };
-
-
 
     return (
             <div className="bg-blue-100 min-h-screen">
